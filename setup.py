58f4265f0caa5b7cfb2d204ee31019dba6f1738f
Yukinari Tani <yukinarit84@gmail.com>
2021-12-10 22:12:57 +0900

    chore: Bump version to 0.1.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ ext = Extension(
 
 setup(
     name="apnggif",
-    version="0.1.3",
+    version="0.1.4",
     description="Python interface to apng2gif",
     long_description=readme,
     long_description_content_type="text/markdown",
